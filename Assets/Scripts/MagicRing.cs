815639a5338c09adc5a27e2f8adb7567ff6893b6
ck1001099 <b03202042@ntu.edu.tw>
2017-05-28 12:56:31 +0800

    magic ring prototype

---
+++ Assets/Scripts/MagicRing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicRing : MonoBehaviour {
+
+	public GameObject basicRing;
+
+	public GameObject signList;
+	public GameObject sign;
+
+	public float Timer;
+	public float timeOffset;
+	public List<float> tempoTime = new List<float> ();
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		Timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Timer = Timer + Time.fixedDeltaTime;
+		basicRing.transform.Rotate (new Vector3 (0, 10, 0) * Time.fixedDeltaTime);
+		if (count < tempoTime.Count && tempoTime [count] < Timer + timeOffset) {
+			CreateSign ();
+			count++;
+		}
+	}
+
+	void CreateSign(){
+		GameObject obj = GameObject.Instantiate (sign);
+		obj.transform.parent = signList.transform;
+		obj.GetComponent<TempoSign> ().SetTimer (timeOffset);
+	}
+}
---
+++ Assets/Scripts/TempoSign.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoSign : MonoBehaviour {
+
+	public float timer;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Mathf.Sqrt (timer)*3;
+		float z = timer;
+		this.transform.localPosition = new Vector3 (x, 0, z);
+		timer = timer - Time.fixedDeltaTime;
+		if (timer < 0) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	public void SetTimer(float iniTime){
+		timer = iniTime;
+	}
+}
